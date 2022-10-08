46288759c0ade2e8d34e9a4affba2de77bac9f27
Steven Munk <s.f.munk@gmx.de>
2022-10-09 00:26:27 +0200

    working on about section

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ let backgroundColors2 = [{ 'normal': '#a7a7a7', 'fire': '#fa8975', 'water': '#7b
 
 
 async function loadPokemon() {
-    let url = `https://pokeapi.co/api/v2/pokemon/aron`;
+    let url = `https://pokeapi.co/api/v2/pokemon/heatran`;
     let response = await fetch(url);
     currentPokemon = await response.json();
 
     renderPokemonInfo();
-    renderPokemonEvolutions();
     console.log('Loaded Pokemon:', currentPokemon);
+    renderPokemonDescription();
 }
 
 function renderPokemonInfo() {
@@ -116,6 +116,121 @@ async function getGermanName() {
 
 
 
+/*********************************************POKEMON ABOUT***********************************************/
+
+async function renderPokemonDescription() {
+
+    let pokemonID = currentPokemon['id']; // The ID (Number) of the Pokemon is teh key
+    let pokemonSpecies = `https://pokeapi.co/api/v2/pokemon-species/${pokemonID}/`;
+    let pokemonSpeciesResponse = await fetch(pokemonSpecies);
+    let pokemonSpeciesAsJson = await pokemonSpeciesResponse.json(); // This JSON contains the species of the Pokemon(this is necessary to get the evolution-chain)
+
+    lookForEnglishDescription(pokemonSpeciesAsJson);
+}
+
+function lookForEnglishDescription(pokemonSpeciesAsJson) {
+    // document.getElementById('content-container').innerHTML = '<div id="pokemon-description"></div>';
+    for (let i = 10; i < 25; i++) {
+        if (pokemonSpeciesAsJson['flavor_text_entries'][`${i}`]['language']['name'] == 'en') {
+            let pokemonDescription = pokemonSpeciesAsJson['flavor_text_entries'][`${i}`]['flavor_text'];
+            document.getElementById('pokemon-description').innerHTML = pokemonDescription;
+        } else {}
+    }
+}
+
+/*********************************************POKEMON ABOUT***********************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -139,6 +254,9 @@ async function renderPokemonEvolutions() {
     let pokemonEvolutionResponse = await fetch(pokemonEvolution);
     let pokemonEvolutionAsJson = await pokemonEvolutionResponse.json(); // This JSON contains the evolution-chain, which means the names of all the pokemons in the ev-chain
 
+    console.log('Pokemon Evolution :', pokemonEvolutionAsJson);
+
+    loadEvolutionHTML();
     renderPokemonFirstStage(pokemonEvolutionAsJson);
     checkIfThereIsMoreThanOneStage(pokemonEvolutionAsJson);
 
@@ -175,6 +293,13 @@ async function renderPokemonFirstStage(pokemonEvolutionAsJson) {
     evolution1AsJSON = await evolution1Response.json();
     let evolution1Artwork = evolution1AsJSON['sprites']['other']['official-artwork']['front_default'];
     document.getElementById('evolution-1').src = evolution1Artwork;
+    getFirstEvolutionName(pokemonEvolutionAsJson);   
+}
+
+function getFirstEvolutionName(pokemonEvolutionAsJson) {
+    let firstLetter = String(pokemonEvolutionAsJson['chain']['species']['name'].charAt(0)).toLocaleUpperCase();
+    firstEvolutionName = firstLetter + pokemonEvolutionAsJson['chain']['species']['name'].slice(1);
+    document.getElementById('first-evolution-name').innerHTML = firstEvolutionName;
 }
 
 async function renderSecondStagePokemon(pokemonEvolutionAsJson) {
@@ -184,6 +309,37 @@ async function renderSecondStagePokemon(pokemonEvolutionAsJson) {
     evolution2AsJSON = await evolution2Response.json();
     let evolution2Artwork = evolution2AsJSON['sprites']['other']['official-artwork']['front_default'];
     document.getElementById('evolution-2').src = evolution2Artwork;
+    getSecondEvolutionName(pokemonEvolutionAsJson)
+    getTriggerForFirstEvolution(pokemonEvolutionAsJson);
+}
+
+function getSecondEvolutionName(pokemonEvolutionAsJson) {
+    let firstLetter = String(pokemonEvolutionAsJson['chain']['evolves_to']['0']['species']['name'].charAt(0)).toLocaleUpperCase();
+    secondEvolutionName = firstLetter + pokemonEvolutionAsJson['chain']['evolves_to']['0']['species']['name'].slice(1);
+    document.getElementById('second-evolution-name').innerHTML = secondEvolutionName;
+}
+
+function getTriggerForFirstEvolution(pokemonEvolutionAsJson) {
+    let evolutionTrigger = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolution_details']['0']['trigger']['name'];
+
+    if (evolutionTrigger == 'level-up') {
+        if (pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolution_details']['0']['min_level']) {
+            let minLvl = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolution_details']['0']['min_level'];
+            document.getElementById('first-evolution').innerHTML = `Lvl. ${minLvl}`;
+        } else {
+            let minFriendship = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolution_details']['0']['min_happiness'];
+            document.getElementById('first-evolution').innerHTML = `Min. Happiness of ${minFriendship}`;
+        }
+    } else {
+        if (evolutionTrigger == 'trade') {
+            document.getElementById('first-evolution').innerHTML = `After a Trade`;
+        } else {
+            if (evolutionTrigger == 'use-item') {
+                let neededItem = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolution_details']['0']['item']['name'];
+                document.getElementById('first-evolution').innerHTML = `Use ${neededItem}`;
+            }
+        }
+    }
 }
 
 async function renderThirdStagePokemon(pokemonEvolutionAsJson) {
@@ -193,15 +349,106 @@ async function renderThirdStagePokemon(pokemonEvolutionAsJson) {
     evolution3AsJSON = await evolution3Response.json();
     let evolution3Artwork = evolution3AsJSON['sprites']['other']['official-artwork']['front_default'];
     document.getElementById('evolution-3').src = evolution3Artwork;
+    getThirdEvolutionName(pokemonEvolutionAsJson);
+    getTriggerForSecondEvolution(pokemonEvolutionAsJson);
+}
+
+function getThirdEvolutionName(pokemonEvolutionAsJson) {
+    let firstLetter = String(pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolves_to']['0']['species']['name'].charAt(0)).toLocaleUpperCase();
+    thirdEvolutionName = firstLetter + pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolves_to']['0']['species']['name'].slice(1);
+    document.getElementById('third-evolution-name').innerHTML = thirdEvolutionName;
+}
+
+function getTriggerForSecondEvolution(pokemonEvolutionAsJson) {
+    let evolutionTrigger = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolves_to']['0']['evolution_details']['0']['trigger']['name'];
+
+    if (evolutionTrigger == 'level-up') {
+        if (pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolves_to']['0']['evolution_details']['0']['min_level']) {
+            let minLvl = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolves_to']['0']['evolution_details']['0']['min_level'];
+            document.getElementById('second-evolution').innerHTML = `Lvl. ${minLvl}`;
+        } else {
+            let minFriendship = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolves_to']['0']['evolution_details']['0']['min_happiness'];
+            document.getElementById('second-evolution').innerHTML = `Min. Happiness of ${minFriendship}`;
+        }
+    } else {
+        if (evolutionTrigger == 'trade') {
+            document.getElementById('second-evolution').innerHTML = `After a Trade`;
+        } else {
+            if (evolutionTrigger == 'use-item') {
+                let neededItem = pokemonEvolutionAsJson['chain']['evolves_to']['0']['evolves_to']['0']['evolution_details']['0']['item']['name'];
+                document.getElementById('second-evolution').innerHTML = `Use ${neededItem}`;
+            }
+        }
+    }
 }
 
 function noSecondOrThirdStage() {
     document.getElementById('evolution-2').src = '';
     document.getElementById('evolution-3').src = '';
+    deleteSecondAndThirdStage();
+}
+
+function deleteSecondAndThirdStage() {
+    let unnecessaryItems = ['first-ev-arrow','second-evolution-name','second-evolution-container','second-ev-trigger','third-evolution-name','third-evolution-container',];
+    for (let i = 0; i < unnecessaryItems.length; i++) {
+        const id = unnecessaryItems[i];
+        document.getElementById(`${id}`).style ='display:none;';
+    }
+    document.getElementById('first-evolution').innerHTML = 'No Futher Evolution'
 }
 
 function noThirdStage() {
     document.getElementById('evolution-3').src = '';
+    let unnecessaryItems = ['second-ev-trigger','third-evolution-name','third-evolution-container',];
+    for (let i = 0; i < unnecessaryItems.length; i++) {
+        const id = unnecessaryItems[i];
+        document.getElementById(`${id}`).style ='display:none;';
+    }
+}
+
+function loadEvolutionHTML() {
+    document.getElementById('content-container').innerHTML = '';
+    document.getElementById('content-container').innerHTML = `
+    
+    <div class="evolution-container">
+
+        <div id="first-evolution-name" class="evolution-name"></div>
+
+        <div class="evolution-img-container">
+            <img id="evolution-1" class="evolution-img" src="">
+            <img class="evolution-img-background" src="icons/pokeball.png">
+        </div>
+
+        <div class="evolution-trigger" style="position: relative; bottom: 55px;">
+            <div id="first-evolution"></div>
+            <img id="first-ev-arrow" src="icons/pfeil-runter.png">
+        </div>
+
+        <div id="second-evolution-name" class="evolution-name" style="position: relative; bottom: 50px;"></div>
+
+        <div id="second-evolution-container" class="evolution-img-container"
+            style="position: relative; bottom: 50px;">
+            <img id="evolution-2" class="evolution-img" src="">
+            <img class="evolution-img-background" src="icons/pokeball.png">
+
+        </div>
+
+        <div id="second-ev-trigger" class="evolution-trigger" style="position: relative; bottom: 100px;">
+            <div id="second-evolution"></div>
+            <img src="icons/pfeil-runter.png">
+        </div>
+
+        <div id="third-evolution-name" class="evolution-name" style="position: relative; bottom: 95px;"></div>
+
+        <div id="third-evolution-container" class="evolution-img-container"
+            style="position: relative; bottom: 95px;">
+            <img id="evolution-3" class="evolution-img" src="">
+            <img class="evolution-img-background" src="icons/pokeball.png">
+        </div>
+
+    </div>
+    
+    `;
 }
 
 
